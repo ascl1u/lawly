c048d0d21418ea3a6c0de377617e599e2d960c25
ascl1u <a.liu27568@gmail.com>
2025-02-17 16:08:48 -0800

    change config again

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,17 @@
 import type { NextConfig } from "next";
-// import path from 'path';
+import path from 'path';
 
 const nextConfig: NextConfig = {
-  // experimental: {
-  //   turbo: {
-  //     rules: {
-  //       '*.worker.min.js': {
-  //         loaders: ['raw-loader'],
-  //         as: '*.js'
-  //       }
-  //     }
-  //   }
-  // },
-  // webpack: (config) => {
-  //   // Preserve the existing aliases
-  //   const existingAlias = config.resolve?.alias || {};
-    
-  //   config.resolve = {
-  //     ...config.resolve,
-  //     alias: {
-  //       ...existingAlias,
-  //       '@': path.join(process.cwd(), './src'),
-  //     },
-  //     // Ensure case sensitivity is enforced
-  //     symlinks: false
-  //   }
-  //   return config
-  // }
+  webpack: (config) => {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve?.alias,
+        '@': path.join(__dirname, 'src').replace(/\\/g, '/'),
+      }
+    }
+    return config
+  }
 };
 
 export default nextConfig;
